bc66a12abe927ac5b180d998cc01150710b0e2e9
Mateus Gomes <mateus.gomes@astrein.com>
2022-05-12 20:06:47 -0300

    Criando filtro da tabela

--- Front/Eventos-App/src/app/eventos/eventos.component.ts
+++ Front/Eventos-App/src/app/eventos/eventos.component.ts
@@ -8,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventosFiltrados : any = [];
   public eventos: any = [];
   margemImagem: number = 2;
   larguraImagem: number = 150;
   exibirImagem: boolean = true;
-  filtroLista: string = '';
-  
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string) : any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== - 1
+    )
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -22,8 +39,11 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get("https://localhost:5001/api/eventos").subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos;
+      },
+        error => console.log(error)
     );
   }
 
