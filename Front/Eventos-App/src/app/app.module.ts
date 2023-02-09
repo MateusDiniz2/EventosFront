8da4a1ca50137801690c40370517efe5f9f1cb50
Mateus Gomes <mateus.gomes@astrein.com>
2023-02-09 08:17:33 -0300

    Feature: Create and organizing components

--- Front/Eventos-App/src/app/app.module.ts
+++ Front/Eventos-App/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 import { DateTimeFormatPipePipe } from './helpers/DateTimeFormatPipe.pipe';
 import { EventoService } from './services/Evento.service';
-
 
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -19,13 +21,18 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { FormsModule } from '@angular/forms';
 import {ToastrModule} from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { TituloComponent } from './shared/titulo/titulo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
       PalestrantesComponent,
+      DashboardComponent,
+      ContatosComponent,
+      PerfilComponent,
       NavComponent,
+      TituloComponent,
       DateTimeFormatPipePipe
    ],
   imports: [
